6ad23c3cdb4e225351a9a19f060e145e5e8b15fb
Miqoilla <Miqoilla@gmail.com>
2024-07-30 00:36:57 +0700

    fourt commit

---
+++ src/app/services/news.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tab4Page } from '../tab4/tab4.page';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+  private apiKey = 'c0aa80ef2cea4128897f332013279965';
+  private apiUrl = 'https://newsapi.org/v2/top-headlines';
+
+  tab4PageInstance: Tab4Page | null = null;
+
+  constructor(private http: HttpClient) { }
+
+  getTopHeadlines(country: string = 'us'): Observable<any> {
+    return this.http.get(`${this.apiUrl}?country=${country}&apiKey=${this.apiKey}`);
+  }
+
+  getCategoryNews(category: string, country: string = 'us'): Observable<any> {
+    return this.http.get(`${this.apiUrl}?country=${country}&category=${category}&apiKey=${this.apiKey}`);
+  }
+
+  searchNews(query: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}?q=${query}&apiKey=${this.apiKey}`);
+  }
+}
